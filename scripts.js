ecd8d9710ebe1f0de4fb46d8ba0852da2282eb86
worsleyj <josephworsley23@gmail.com>
2024-07-18 14:07:53 -0400

    basic functions and buttons added

---
+++ scripts.js
@@ -0,0 +1,33 @@
+let num1 = 0;
+let num2 = 0;
+let operator = 0;
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    if (operator == "+") {
+        return add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2);
+    } else if (operator == "*") {
+        return multiply(num1, num2);
+    } else if (operator == "/") {
+        return divide(num1, num2);
+    } else return "INVALID OPERATOR";
+}
+
+console.log(operate("*", 4, 5));
