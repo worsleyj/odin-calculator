d4db125afbe84d173764c96f9184a624767a3587
worsleyj <josephworsley23@gmail.com>
2024-07-18 14:18:29 -0400

    operator buttons hooked up

--- scripts.js
+++ scripts.js
@@ -1,6 +1,28 @@
 let num1 = 0;
 let num2 = 0;
-let operator = 0;
+let operator = "";
+
+const addBtn = document.querySelector(".add-button");
+const subtractBtn = document.querySelector(".subtract-button");
+const multiplyBtn = document.querySelector(".multiply-button");
+const divideBtn = document.querySelector(".divide-button");
+
+addBtn.addEventListener("click", () => {
+    operator = "+";
+})
+
+subtractBtn.addEventListener("click", () => {
+    operator = "-";
+})
+
+multiplyBtn.addEventListener("click", () => {
+    operator = "*";
+})
+
+divideBtn.addEventListener("click", () => {
+    operator = "/";
+})
+
 
 function add(num1, num2){
     return num1 + num2;
@@ -29,5 +51,6 @@ function operate(operator, num1, num2) {
         return divide(num1, num2);
     } else return "INVALID OPERATOR";
 }
+
 
 console.log(operate("*", 4, 5));
